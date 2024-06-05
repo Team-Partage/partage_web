fed0e7339d2e810c7e41b4b6b3755800e29dc06a
noismik <noismik97@gmail.com>
2024-06-05 19:08:50 +0900

    feat: Fetcher http method 추가

--- src/lib/fetcher/index.ts
+++ src/lib/fetcher/index.ts
@@ -1,9 +1,9 @@
+type FetcherRequestInit = Omit<RequestInit, 'method'>;
+
 type Config = {
   baseURL?: string;
-  defaultRequestInit?: RequestInit;
+  defaultRequestInit?: FetcherRequestInit;
 };
-
-// type FetcherRequestInit = Omit<RequestInit, 'method'>;
 
 // type DeepPartial<T> = {
 //   [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
@@ -13,38 +13,134 @@ type Config = {
 
 class Fetcher {
   private baseURL: string | URL | undefined;
-  private defaultRequestInit: RequestInit | undefined;
+  private defaultRequestInit: FetcherRequestInit | undefined;
+
   constructor(config?: Config) {
     this.baseURL = config?.baseURL;
     this.defaultRequestInit = config?.defaultRequestInit;
   }
 
-  // async get<T>(endpoint: string, options?: FetcherRequestInit): Promise<T> {
-  //   try {
-  //     const url = new URL(endpoint, this.baseURL);
+  async get<T>(endpoint: string): Promise<T> {
+    try {
+      const url = new URL(endpoint, this.baseURL);
 
-  //     const res = await fetch(url, {
-  //       method: 'GET',
-  //     });
+      const res = await fetch(url, {
+        ...this.defaultRequestInit,
+        method: 'GET',
+      });
 
-  //     if (!res.ok) {
-  //       throw new Error(`HTTP ERROR status: ${res.status}`);
-  //     }
+      if (!res.ok) {
+        throw new Error(`HTTP ERROR status: ${res.status}`);
+      }
 
-  //     const data: T = await res.json();
+      const data: T = await res.json();
 
-  //     return data;
-  //   } catch (error) {
-  //     if (process.env.NODE_ENV === 'development') {
-  //       console.error(error);
-  //     }
-  //     return Promise.reject(error);
-  //   }
-  // }
-  // async post<T>(endpoint: string, params: object): Promise<T> {}
-  // async put<T>(endpoint: string, params: object): Promise<T> {}
-  // async patch<T>(endpoint: string): Promise<T> {}
-  // async delete<T>(endpoint: string): Promise<T> {}
+      return data;
+    } catch (error) {
+      if (process.env.NODE_ENV === 'development') {
+        console.error(error);
+      }
+      return Promise.reject(error);
+    }
+  }
+  async post<T>(endpoint: string, params: object): Promise<T> {
+    try {
+      const url = new URL(endpoint, this.baseURL);
+
+      const res = await fetch(url, {
+        ...this.defaultRequestInit,
+        method: 'POST',
+        body: JSON.stringify(params),
+      });
+
+      if (!res.ok) {
+        throw new Error(`HTTP ERROR status: ${res.status}`);
+      }
+
+      const data: T = await res.json();
+
+      return data;
+    } catch (error) {
+      if (process.env.NODE_ENV === 'development') {
+        console.error(error);
+      }
+      return Promise.reject(error);
+    }
+  }
+  async put<T>(endpoint: string, params: object): Promise<T> {
+    try {
+      const url = new URL(endpoint, this.baseURL);
+
+      const res = await fetch(url, {
+        ...this.defaultRequestInit,
+        method: 'PUT',
+        body: JSON.stringify(params),
+      });
+
+      if (!res.ok) {
+        throw new Error(`HTTP ERROR status: ${res.status}`);
+      }
+
+      const data: T = await res.json();
+
+      return data;
+    } catch (error) {
+      if (process.env.NODE_ENV === 'development') {
+        console.error(error);
+      }
+      return Promise.reject(error);
+    }
+  }
+  async patch<T>(endpoint: string, params: object): Promise<T> {
+    try {
+      const url = new URL(endpoint, this.baseURL);
+
+      const res = await fetch(url, {
+        ...this.defaultRequestInit,
+        method: 'PATCH',
+        body: JSON.stringify(params),
+      });
+
+      if (!res.ok) {
+        throw new Error(`HTTP ERROR status: ${res.status}`);
+      }
+
+      const data: T = await res.json();
+
+      return data;
+    } catch (error) {
+      if (process.env.NODE_ENV === 'development') {
+        console.error(error);
+      }
+      return Promise.reject(error);
+    }
+  }
+  async delete<T>(endpoint: string): Promise<T> {
+    try {
+      const url = new URL(endpoint, this.baseURL);
+
+      const res = await fetch(url, {
+        ...this.defaultRequestInit,
+        method: 'DELETE',
+      });
+
+      if (!res.ok) {
+        throw new Error(`HTTP ERROR status: ${res.status}`);
+      }
+
+      const data: T = await res.json();
+
+      return data;
+    } catch (error) {
+      if (process.env.NODE_ENV === 'development') {
+        console.error(error);
+      }
+      return Promise.reject(error);
+    }
+  }
 }
 
-export const fetcher = new Fetcher();
+export const fetcher = new Fetcher({
+  baseURL: 'http://localhost:9090',
+  defaultRequestInit: { headers: { 'Content-Type': 'application/json' } },
+});
