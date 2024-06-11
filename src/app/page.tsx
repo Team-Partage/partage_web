55c68737a4a6c7b252739afdc52cdf462d7f3384
noismik <noismik97@gmail.com>
2024-06-11 17:28:56 +0900

    feat: 메인 페이지 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import Main from './main/page';
+import { Button } from '@/components/ui/button';
+import { getChannelList } from '@/services/channel';
+import { Plus } from 'lucide-react';
 
-export default function Home() {
+const Home = async () => {
+  const channels = await getChannelList();
+
+  if (channels.page.total_count === 0) {
+    return (
+      <div className="border border-dashed">
+        <p>생성된 채널이 없어서 조용하네요!</p>
+        <Button variant="active" className="px-4 base-bold">
+          <Plus width={20} height={20} strokeWidth={2} />
+          채널 생성
+        </Button>
+      </div>
+    );
+  }
   return (
-    <div className="w-full pt-[36px]">
-      <Main />
+    <div className="flex h-[400px] min-h-[350px] min-w-[280px] max-w-[800px] flex-col items-center justify-center rounded-lg border border-dashed border-[#43B0FF] mobile:max-w-[280px] tablet:h-[350px] tablet:max-w-[532px]">
+      <div className="flex flex-col items-center gap-12">
+        <p>생성된 채널이 없어서 조용하네요!</p>
+        <Button variant="active" className="w-full px-4 base-bold">
+          <Plus width={20} height={20} strokeWidth={2} />
+          채널 생성
+        </Button>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
--- src/services/channel/index.ts
+++ src/services/channel/index.ts
@@ -1,1 +1,9 @@
+import { fetcher } from '@/lib/fetcher';
+
+import { GetChannelListResponse } from './type';
+
 /** */
+export const getChannelList = async (params?: { cursor?: number; perPage?: number }) => {
+  const data = await fetcher.get<GetChannelListResponse>('/api/v1/channel/search', params || {});
+  return data;
+};
