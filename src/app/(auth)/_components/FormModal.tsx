ae6d2bf8c3509094f0c68f0404d138d6a874db87
Marais <maraisienne.lee@gmail.com>
2024-06-11 15:21:27 +0900

    feat: 회원가입 페이지 마크업 및 form 유효성 검사

--- src/app/(auth)/_components/FormModal.tsx
+++ src/app/(auth)/_components/FormModal.tsx
@@ -1,22 +1,16 @@
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogHeader, DialogTrigger } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader } from '@/components/ui/dialog';
 
 type Props = {
-  title: '로그인' | '회원가입';
   content: string;
+  open: boolean;
+  setOpen: (open: boolean) => void;
   handleModal: () => void;
 };
 
-const FormModal = ({ title, content, handleModal }: Props) => {
+const FormModal = ({ content, open, setOpen, handleModal }: Props) => {
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <div className="w-full">
-          <Button type="submit" variant="active" size="lg" className="mt-[56px] w-full">
-            {title}
-          </Button>
-        </div>
-      </DialogTrigger>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="h-[237px] w-[400px] rounded-[8px] border-0 mobile:h-[198px] mobile:w-[335px] ">
         <DialogHeader className="pt-[80px] text-neutral-100 medium-regular mobile:pt-[64px] mobile:base-regular">
           {content}
