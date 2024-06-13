452650aeed2335ed5f94ff42d912228aa103f635
noismik <noismik97@gmail.com>
2024-06-13 20:02:37 +0900

    fix(env.d.ts): 오타 수정

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_SERVER_URL: string;
-    NEXT_PUBLICK_WEBSOCKET_SERVER_URL: string;
+    NEXT_PUBLIC_WEBSOCKET_SERVER_URL: string;
   }
 }
