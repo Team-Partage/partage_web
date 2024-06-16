d92b1dc8d6340329422b3587034b96dd154e0d0e
Marais <maraisienne.lee@gmail.com>
2024-06-16 18:20:51 +0900

    feat: 회원탈퇴 기능

--- src/app/mypage/_components/WithDraw.tsx
+++ src/app/mypage/_components/WithDraw.tsx
@@ -1,12 +1,19 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
+import { Withdrawal } from '@/services/user';
 
 const WithDraw = () => {
+  const handleWithDraw = async () => {
+    await Withdrawal();
+  };
+
   return (
-    <Card className='mb-[140px] border-0'>
+    <Card className="mb-[140px] border-0">
       <CardTitle>회원 탈퇴</CardTitle>
       <CardContent>
-        <Button variant="withDraw" size="lg" className="w-full">
+        <Button variant="withDraw" size="lg" className="w-full" onClick={handleWithDraw}>
           회원 탈퇴
         </Button>
       </CardContent>
