a48c151bc450b92cb7de72dd5f2b1c1c43bf2ad4
Marais <maraisienne.lee@gmail.com>
2024-06-16 18:34:08 +0900

    fix: myPage 페이지용 layout 생성

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -4,12 +4,10 @@ import WithDraw from './_components/WithDraw';
 
 const Mypage = () => {
   return (
-    <div className="mt-[40px] flex w-full flex-col items-center justify-center ">
-      <div className="flex w-[640px] flex-col gap-[100px]">
-        <EditMyInfo />
-        <PasswordCheck />
-        <WithDraw />
-      </div>
+    <div className="flex w-[640px] flex-col gap-[100px]">
+      <EditMyInfo />
+      <PasswordCheck />
+      <WithDraw />
     </div>
   );
 };
