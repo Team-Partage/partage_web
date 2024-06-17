320d14b298cdf24208a53bfd0e29cca43df3c80f
sooki88 <sookhee9n@gmail.com>
2024-06-17 17:31:29 +0900

    Chore: 사용하지 않는 코드 삭제

--- src/utils/searchRoute.ts
+++
@@ -1,7 +0,0 @@
-import { type NextRequest } from 'next/server';
-
-export function getSearchQuery(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const query = searchParams.get(`query`);
-  return query;
-}
