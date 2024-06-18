444d57b1a1f5d8e80c15ba237fdfdbbfb8e1f208
Marais <maraisienne.lee@gmail.com>
2024-06-18 18:16:01 +0900

    comment: API 별 주석 추가

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -4,27 +4,31 @@ import { EditProfileParams, NicknameRequest, SignUpRequest } from './type';
 
 const baseUrl = '/api/v1/user';
 
-// 일반 회원가입
+//** 일반 회원가입 */ 
 export const SignUp = async (params: SignUpRequest) => {
   const data = await fetcher.post(`${baseUrl}/join`, params);
   return data;
 };
 
+//** 회원 탈퇴 */ 
 export const Withdrawal = async () => {
   const data = await fetcher.delete(`${baseUrl}/me`);
   return data;
 };
 
+//** 닉네임 수정 */ 
 export const CheckNickname = async (params: NicknameRequest) => {
   const data = await fetcher.post(`${baseUrl}/check-nickname`, params);
   return data;
 };
 
+//** 프로필 수정 (닉네임, 색상, 비밀번호) */ 
 export const EditProfile = async <T extends EditProfileParams>(endpoint: string, params: T) => {
   const data = await fetcher.patch(`${baseUrl}/me/${endpoint}`, params);
   return data;
 };
 
+//** 프로필 이미지 수정 */ 
 export const EditProfileImage = async (params: FormData) => {
   const data = await fetcher.post(`${baseUrl}/me/profile-image`, params);
   return data;
