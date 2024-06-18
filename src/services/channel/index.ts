4b607f003a923b99659e7216fa28e6333805e768
noismik <noismik97@gmail.com>
2024-06-18 14:45:26 +0900

    feat: 채널 상세조회, 플레이리스트 조회 api

--- src/services/channel/index.ts
+++ src/services/channel/index.ts
@@ -1,9 +1,21 @@
 import { fetcher } from '@/lib/fetcher';
 
-import { GetChannelListResponse } from './type';
+import { GetChannelDetailResponse, GetChannelListResponse } from './type';
 
 /** */
 export const getChannelList = async (params?: { cursor?: number; perPage?: number }) => {
   const data = await fetcher.get<GetChannelListResponse>('/api/v1/channel/search', params || {});
+  return data;
+};
+
+/** 채널 상세 정보 조회 */
+export const getChannelDetail = async (params: {
+  channelId: string;
+  page?: number;
+  pageSize?: number;
+}) => {
+  const { channelId, ...rest } = params;
+
+  const data = await fetcher.get<GetChannelDetailResponse>(`/api/v1/channel/${channelId}`, rest);
   return data;
 };
--- src/services/playlist/type.ts
+++ src/services/playlist/type.ts
@@ -1,3 +1,4 @@
+/** 플레이리스트 */
 export type Playlist = {
   playlist_no: number;
   channel_id: string;
@@ -6,4 +7,12 @@ export type Playlist = {
   url: string;
   thumbnail: string;
   create_at: string;
+};
+
+/** 플레이리스트 조회 */
+export type GetPlaylistResponse = {
+  playlist: Playlist[];
+  total_contents: number;
+  page_size: number;
+  page: number;
 };
