601154edb88363541d22ed0e468a65d53403723e
Marais <maraisienne.lee@gmail.com>
2024-06-21 15:42:40 +0900

    feat: user 관련 API 요청 기능

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -1,35 +1,65 @@
 import { fetcher } from '@/lib/fetcher';
 
-import { EditProfileParams, NicknameRequest, SignUpRequest } from './type';
+import {
+  CheckEmailNumberRequest,
+  EditProfileParams,
+  EmailRequest,
+  NicknameRequest,
+  SignUpRequest,
+} from './type';
 
 const baseUrl = '/api/v1/user';
 
-//** 일반 회원가입 */ 
+//** 일반 회원가입 */
 export const SignUp = async (params: SignUpRequest) => {
   const data = await fetcher.post(`${baseUrl}/join`, params);
   return data;
 };
 
-//** 회원 탈퇴 */ 
+//** 회원 탈퇴 */
 export const Withdrawal = async () => {
   const data = await fetcher.delete(`${baseUrl}/me`);
   return data;
 };
 
-//** 닉네임 수정 */ 
+//** 닉네임 중복 확인 */
 export const CheckNickname = async (params: NicknameRequest) => {
   const data = await fetcher.post(`${baseUrl}/check-nickname`, params);
   return data;
 };
 
-//** 프로필 수정 (닉네임, 색상, 비밀번호) */ 
+//** 프로필 수정 (닉네임, 색상, 비밀번호) */
 export const EditProfile = async <T extends EditProfileParams>(endpoint: string, params: T) => {
   const data = await fetcher.patch(`${baseUrl}/me/${endpoint}`, params);
   return data;
 };
 
-//** 프로필 이미지 수정 */ 
+//** 프로필 이미지 수정 */
 export const EditProfileImage = async (params: FormData) => {
   const data = await fetcher.post(`${baseUrl}/me/profile-image`, params);
+  return data;
+};
+
+//** 이메일 중복 확인 */
+export const CheckEmail = async (params: EmailRequest) => {
+  const data = await fetcher.post(`${baseUrl}/check-email`, params);
+  return data;
+};
+
+//** 이메일 인증 메일 전송 */
+export const SendEmail = async (params: EmailRequest) => {
+  const data = await fetcher.post(`${baseUrl}/auth-email`, params);
+  return data;
+};
+
+//** 이메일 인증 번호 확인 */
+export const CheckEmailNumber = async (params: CheckEmailNumberRequest) => {
+  const data = await fetcher.post(`${baseUrl}/auth-number`, params);
+  return data;
+};
+
+//** 비밀번호 찾기 */
+export const FindPassword = async (params: EmailRequest) => {
+  const data = await fetcher.post(`${baseUrl}/password`, params);
   return data;
 };
--- src/services/user/type.ts
+++ src/services/user/type.ts
@@ -17,15 +17,11 @@ export type SignUpRequest = {
   password: string;
 } & Pick<User, 'email' | 'username' | 'nickname'>;
 
-//** 프로필 색상 수정 Req */
-export type EditProfileColorRequest = {
-  profile_color: string;
-};
+//** 닉네임 Req */
+export type NicknameRequest = Pick<User, 'nickname'>;
 
-//** 닉네임 수정 Req */
-export type NicknameRequest = {
-  nickname: string;
-};
+//** 프로필 색상 수정 Req */
+export type EditProfileColorRequest = Pick<User, 'profile_color'>;
 
 //** 비밀번호 수정 Req */
 export type EditPasswordRequest = {
@@ -35,3 +31,9 @@ export type EditPasswordRequest = {
 
 //** 프로필 수정 Params */
 export type EditProfileParams = EditProfileColorRequest | NicknameRequest | EditPasswordRequest;
+
+//** 이메일 Req */
+export type EmailRequest = Pick<User, 'email'>;
+
+//** 이메일 인증 번호 확인 Req */
+export type CheckEmailNumberRequest = { auth_number: string } & EmailRequest;
