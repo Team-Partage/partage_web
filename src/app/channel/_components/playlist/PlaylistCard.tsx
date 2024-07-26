1367a6ebde98c3ca7ae0a07055c3b84a1e4ee953
noismik <noismik97@gmail.com>
2024-07-26 14:32:38 +0900

    design: 플레이리스트 줄바꿈

--- src/app/channel/_components/playlist/PlaylistCard.tsx
+++ src/app/channel/_components/playlist/PlaylistCard.tsx
@@ -17,7 +17,7 @@ const PlaylistCard = ({ thumbnail, title }: Props) => {
         height={42}
         alt="thumbnail"
       />
-      <p className="ml-4 text-neutral-100 small-regular">{title}</p>
+      <p className="ml-4 line-clamp-2 text-neutral-100 small-regular">{title}</p>
     </div>
   );
 };
