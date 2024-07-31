fcd5551b35f159f4a5c9cb2579fa09009bf91281
noismik <noismik97@gmail.com>
2024-07-31 15:26:56 +0900

    feat(usePermissionStore): 사용자 권한등급 추가 및 적용

--- src/stores/usePermissionStore.ts
+++ src/stores/usePermissionStore.ts
@@ -1,8 +1,17 @@
 import { ChannelPermission, RoleIdType } from '@/services/channel/type';
+import checkPermission from '@/utils/CheckPermission';
 import { create } from 'zustand';
 
+type Permission = {
+  [key in keyof ChannelPermission]: boolean;
+};
 interface PermissionStore {
   roleId: RoleIdType | null;
+
+  /** 사용자 권한 */
+  permission: Permission;
+
+  /** 채널 권한 */
   channelPermission: ChannelPermission;
 
   setRoleId: (roleId: RoleIdType) => void;
@@ -12,6 +21,17 @@ interface PermissionStore {
 
 const INITIAL_DATA: Omit<PermissionStore, 'setRoleId' | 'setChannelPermission' | 'reset'> = {
   roleId: null,
+  permission: {
+    playlist_add: false,
+    playlist_remove: false,
+    playlist_move: false,
+    video_play: false,
+    video_seek: false,
+    video_skip: false,
+    chat_send: false,
+    chat_delete: false,
+    ban: false,
+  },
   channelPermission: {
     playlist_add: 'C0000',
     playlist_remove: 'C0000',
@@ -27,12 +47,24 @@ const INITIAL_DATA: Omit<PermissionStore, 'setRoleId' | 'setChannelPermission' |
 
 export const usePermissionStore = create<PermissionStore>((set) => ({
   roleId: INITIAL_DATA.roleId,
+  permission: INITIAL_DATA.permission,
   channelPermission: INITIAL_DATA.channelPermission,
 
   setRoleId: (roleId) => set({ roleId }),
-  setChannelPermission: (channelPermission) => set({ channelPermission }),
+  setChannelPermission: (channelPermission) => {
+    set({ channelPermission });
+    set((state) => {
+      const newPermission: Permission = state.permission;
+
+      for (const key in state.channelPermission) {
+        newPermission[key as keyof ChannelPermission] = checkPermission(
+          key as keyof ChannelPermission,
+        );
+      }
+      return { permission: newPermission };
+    });
+  },
   reset: () => {
-    console.log('rest');
     set(INITIAL_DATA);
   },
 }));
