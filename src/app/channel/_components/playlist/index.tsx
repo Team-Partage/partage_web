43d70e70edffd00a2b7dcaa0d5e170e3cb3daa62
noismik <noismik97@gmail.com>
2024-08-07 09:32:30 +0900

    feat: 플레이리스트 삭제 시 다음 영상 재생

--- src/app/channel/_components/playlist/index.tsx
+++ src/app/channel/_components/playlist/index.tsx
@@ -55,6 +55,7 @@ const Playlist = ({ channel }: Props) => {
   const handleDelete = (id: number) => {
     if (!permission.playlist_remove) return;
     send('PLAYLIST_REMOVE', { playlist_no: id });
+    nextVideo();
   };
 
   /** 플레이리스트 추가 */
